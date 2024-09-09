073b51977ca7eddc16790d9a5a982929f2740495
Julius <th3julius@gmail.com>
2024-09-09 13:44:36 -0500

    primer commmit

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>laravel 11</title>
+</head>
+<body>
+  <h1>show posts {{$post}} </h1>
+  @if(false)
+    <p>prueba</p>
+  @endif
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+
+Route::get('/', HomeController::class);
+
+
+Route::get('/posts', [PostController::class, 'index']);
+
+Route::get('/posts/create', [PostController::class, 'create']);
+
+Route::get('/posts/{post}', [PostController::class, 'show']);
+
+
+// Route::get('/posts/{post}', function($post){
+//     return "posts view {$post}";
+// });
